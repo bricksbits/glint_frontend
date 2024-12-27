085a267e0e9370899bd97f1fb305a16338225091
Abhishek <abhishekverma7t@gmail.com>
2024-12-27 13:29:56 +0530

    Notification screen main banner Completed

--- lib/features/notifications/notification_screen.dart
+++ lib/features/notifications/notification_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:glint_frontend/design/exports.dart';
 
 class NotificationScreen extends StatelessWidget {
@@ -6,15 +7,64 @@ class NotificationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: GlintCustomAppBar(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: const GlintCustomAppBar(
         title: 'Notifications',
         subtitle: 'Never miss a moment that matters.',
       ),
-      body: Center(
-        child: Text(
-            'Will show all the upto last week notifications, and respective events navigation logic'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 18.0,
+        ),
+        child: Column(
+          children: [
+            _buildNotificationScreenBanner(),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _buildNotificationScreenBanner() {
+    return Stack(
+      children: [
+        SvgPicture.asset(
+          'lib/assets/images/notification_container_illustration.svg',
+          alignment: Alignment.center,
+          width: double.infinity,
+          height: 120.0,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 22.0,
+            vertical: 24.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Want More Matches?',
+                style: AppTheme.headingThree.copyWith(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
+              const SizedBox(height: 8.0),
+              Text(
+                'Keep your profile\nupdated and active!',
+                style: AppTheme.simpleText.copyWith(
+                  color: Colors.white,
+                  fontSize: 12.0,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
