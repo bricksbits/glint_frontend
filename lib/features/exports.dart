d82dbdc2f8ae83bb594e5bdb32ae6d7f60b90c50
Abhishek <abhishekverma7t@gmail.com>
2024-12-28 16:50:33 +0530

    Setup: Notification Screen + Main Card / Asset import

--- lib/features/exports.dart
+++ lib/features/exports.dart
@@ -1,3 +1,4 @@
 export 'application/my_glint_app.dart';
+export 'filter/filter_preference_screen.dart';
 export 'likes/likes_screen.dart';
 export 'notifications/notification_screen.dart';
--- lib/features/filter/filter_preference_screen.dart
+++ lib/features/filter/filter_preference_screen.dart
@@ -1,13 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gap/gap.dart';
+import 'package:glint_frontend/design/exports.dart';
 
 class FilterPreferenceScreen extends StatelessWidget {
   const FilterPreferenceScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-          'Filter Screen, where the last value will get save to shared Prefs'),
+    return Scaffold(
+      backgroundColor: AppColours.scaffoldBackgroundColor,
+      appBar: AppBar(
+        backgroundColor: AppColours.scaffoldBackgroundColor,
+      ), // for design replication purpose
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20.0,
+        ),
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(
+              20.0,
+            ),
+            image: const DecorationImage(
+              image: AssetImage(
+                'lib/assets/images/filter_card_illustration.png',
+              ),
+              fit: BoxFit.fill,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                'lib/assets/icons/filter_card_main_icon.svg',
+                alignment: Alignment.center,
+                width: 52.0,
+                height: 52.0,
+              ),
+              const Gap(10.0),
+              Text(
+                'It’s a little quiet here',
+                style: AppTheme.headingTwo.copyWith(
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
+              const Gap(10.0),
+              const Text(
+                'Try updating your preferences\n or boosting your profile!',
+                textAlign: TextAlign.center,
+                style: AppTheme.simpleText,
+              ),
+              const Gap(60.0),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: const SizedBox(
+        height: 120.0,
+      ),
     );
   }
 }
