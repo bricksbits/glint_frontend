674e3e1f1cfeb1d3546400c40092d710557952a5
Abhishek <abhishekverma7t@gmail.com>
2025-01-05 18:35:03 +0530

    Completed: Screen for pronouns and looking for card

--- lib/features/onboarding/get_set_glint_onboarding_screen.dart
+++ lib/features/onboarding/get_set_glint_onboarding_screen.dart
@@ -94,8 +94,10 @@ class _GetSetGlintOnboardingScreenState
           padding: const EdgeInsets.symmetric(horizontal: 20.0)
               .copyWith(bottom: 16.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
             children: [
-              // heading
               RichText(
                 textAlign: TextAlign.center,
                 text: TextSpan(
@@ -122,16 +124,31 @@ class _GetSetGlintOnboardingScreenState
                 'Set your vibe and let the sparks fly!',
                 style: AppTheme.smallBodyText,
               ),
+              const Gap(60.0),
 
-              const Gap(32.0),
-
-              // look for card
               _buildLookingForCard(),
 
               const Gap(16.0),
 
               // pronouns card
               _buildPronounsWidget(),
+
+              const Spacer(
+                flex: 12,
+              ),
+
+              // submit button
+              SizedBox(
+                width: double.infinity,
+                child: GlintElevatedButton(
+                  label: 'Next',
+                  foregroundColor: Colors.white,
+                  backgroundColor: AppColours.primaryBlue,
+                  onPressed: () {
+                    // TODO: Handle navigation or next step
+                  },
+                ),
+              ),
             ],
           ),
         ),
