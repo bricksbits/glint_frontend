e211acbbfdd799f4beef55a43ab471b03d52d95d
Abhishek <abhishekverma7t@gmail.com>
2025-01-06 17:13:18 +0530

    Added: Responsiveness to GetSetGlint screen + Added high five illustration

--- lib/features/application/my_glint_app.dart
+++ lib/features/application/my_glint_app.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:glint_frontend/design/exports.dart';
-import 'package:glint_frontend/features/auth/authentication_screen.dart';
-import 'package:glint_frontend/navigation/glint_route_config.dart';
+import 'package:glint_frontend/features/onboarding/get_set_glint_onboarding_screen.dart';
 
 class MyGlintApp extends StatelessWidget {
   const MyGlintApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
+    return MaterialApp(
       title: "Glint People App",
       debugShowCheckedModeBanner: false,
       theme: AppTheme.theme,
-      routerConfig: glintMainRoutes,
+      home: const GetSetGlintOnboardingScreen(),
+      // routerConfig: glintMainRoutes,
       // routerConfig: glintMainRoutes,
     );
   }
