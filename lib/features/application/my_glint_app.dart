7844e4c0b91e91eb4d48adf68f0f460d7ff2a9e3
Abhishek <abhishekverma7t@gmail.com>
2025-01-10 22:50:15 +0530

    Completed: UI for User Profile Screen / Setup in Main Screen Bottom nav bar

--- lib/features/application/my_glint_app.dart
+++ lib/features/application/my_glint_app.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:glint_frontend/design/exports.dart';
-import 'package:glint_frontend/features/profile/profile_screen.dart';
+import 'package:glint_frontend/navigation/glint_route_config.dart';
 
 class MyGlintApp extends StatelessWidget {
   const MyGlintApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: "Glint People App",
       debugShowCheckedModeBanner: false,
       theme: AppTheme.theme,
-      home: const ProfileScreen(),
-      // routerConfig: glintMainRoutes,
+      routerConfig: glintMainRoutes,
       // routerConfig: glintMainRoutes,
     );
   }
