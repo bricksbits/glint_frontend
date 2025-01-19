5aa8d06c4474bcc48f29cda2c58da164fe54fe98
Abhishek <abhishekverma7t@gmail.com>
2025-01-19 18:27:16 +0530

    enum setup for custom app bar actions based on screen

--- lib/design/components/glint_app_bar.dart
+++ lib/design/components/glint_app_bar.dart
@@ -5,46 +5,51 @@ import 'package:glint_frontend/design/exports.dart';
 import 'package:glint_frontend/navigation/glint_all_routes.dart';
 import 'package:go_router/go_router.dart';
 
+enum GlintAppBarActions {
+  defaultActions,
+  profile,
+  event,
+}
+
 class GlintAppBar extends StatelessWidget implements PreferredSizeWidget {
   const GlintAppBar({
     super.key,
-    this.isProfileScreen = false,
+    this.appBarAction = GlintAppBarActions.defaultActions,
   });
 
-  final bool? isProfileScreen;
+  final GlintAppBarActions appBarAction;
 
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      backgroundColor: AppColours.white,
-      scrolledUnderElevation: 0,
-      centerTitle: false,
-      titleSpacing: 0,
-      title: Padding(
-        padding: EdgeInsets.only(
-          left: isProfileScreen == true ? 24.0 : 20.0,
-        ),
-        child: SvgPicture.asset(
-          'lib/assets/icons/glint_logo.svg',
-        ),
-      ),
-      actions: [
-        if (isProfileScreen == true) ...[
+  List<Widget> getAppBarActions(BuildContext context) {
+    switch (appBarAction) {
+      case GlintAppBarActions.profile:
+        return [
           GestureDetector(
             onTap: () {
-              //todo - rollback functionality
+              // todo - add settings clicked functionality
             },
             child: SvgPicture.asset(
               'lib/assets/icons/settings_icon.svg',
             ),
           ),
-          const Gap(4.0),
-        ],
-
-        if (isProfileScreen == false) ...[
+          const Gap(20.0),
+        ];
+      case GlintAppBarActions.event:
+        return [
           GestureDetector(
             onTap: () {
-              //todo - rollback functionality
+              context.pushNamed(GlintMainRoutes.filter.name);
+            },
+            child: SvgPicture.asset(
+              'lib/assets/icons/glint_filter.svg',
+            ),
+          ),
+          const Gap(20.0),
+        ];
+      case GlintAppBarActions.defaultActions:
+        return [
+          GestureDetector(
+            onTap: () {
+              // Replace with rollback functionality
             },
             child: SvgPicture.asset(
               'lib/assets/icons/glint_rollback.svg',
@@ -77,14 +82,28 @@ class GlintAppBar extends StatelessWidget implements PreferredSizeWidget {
               'lib/assets/icons/glint_filter.svg',
             ),
           ),
-        ],
+          const Gap(20.0),
+        ];
+    }
+  }
 
-        const Gap(20.0), // for design replication purpose
-      ],
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: AppColours.white,
+      scrolledUnderElevation: 0,
+      centerTitle: false,
+      titleSpacing: 0,
+      title: Padding(
+        padding: const EdgeInsets.only(left: 20.0),
+        child: SvgPicture.asset(
+          'lib/assets/icons/glint_logo.svg',
+        ),
+      ),
+      actions: getAppBarActions(context),
     );
   }
 
   @override
-  // TODO: implement preferredSize
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
