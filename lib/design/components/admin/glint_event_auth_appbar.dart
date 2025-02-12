a103fa1ab43f013140d98b2d9c700490b1659e30
Abhishek <abhishekverma7t@gmail.com>
2025-02-12 18:58:45 +0530

    Completed: UI for admin log out pop up

--- lib/design/components/admin/glint_event_auth_appbar.dart
+++ lib/design/components/admin/glint_event_auth_appbar.dart
@@ -3,7 +3,7 @@ import 'package:flutter_svg/svg.dart';
 import 'package:gap/gap.dart';
 import 'package:glint_frontend/design/exports.dart';
 
-class GlintEventAuthAppbar extends StatelessWidget
+class GlintEventAuthAppbar extends StatefulWidget
     implements PreferredSizeWidget {
   const GlintEventAuthAppbar({
     super.key,
@@ -11,6 +11,87 @@ class GlintEventAuthAppbar extends StatelessWidget
   });
 
   final bool hasLogoutIcon;
+
+  @override
+  State<GlintEventAuthAppbar> createState() => _GlintEventAuthAppbarState();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class _GlintEventAuthAppbarState extends State<GlintEventAuthAppbar> {
+  void _showLogoutDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+          ),
+          child: Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 28.0,
+              vertical: 32.0,
+            ),
+            decoration: BoxDecoration(
+              color: AppColours.white,
+              borderRadius: BorderRadius.circular(12.0),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Icon(
+                  Icons.power_settings_new_outlined,
+                  color: AppColours.red,
+                  size: 24.0,
+                ),
+                const Gap(12.0),
+                const Text(
+                  "Logout Account",
+                  style: AppTheme.heavyBodyText,
+                ),
+                const Text(
+                  "Are you sure you want to logout?",
+                  style: AppTheme.simpleText,
+                ),
+                const Gap(60.0),
+                SizedBox(
+                  width: 300,
+                  height: 56.0,
+                  child: GlintElevatedButton(
+                    label: 'Yes, Logout',
+                    onPressed: () {
+                      // todo - add logout functionality
+                    },
+                    customTextStyle: AppTheme.simpleBodyText.copyWith(
+                      color: AppColours.white,
+                    ),
+                    backgroundColor: AppColours.pink,
+                    customBorderRadius: 10.0,
+                  ),
+                ),
+                SizedBox(
+                  width: 300,
+                  height: 56.0,
+                  child: GlintElevatedButton(
+                    label: 'Cancel',
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    customTextStyle: AppTheme.simpleBodyText.copyWith(
+                      fontWeight: FontWeight.w400,
+                    ),
+                    backgroundColor: Colors.transparent,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +102,7 @@ class GlintEventAuthAppbar extends StatelessWidget
         'lib/assets/images/admin/glint_event_management_logo.svg',
       ),
       titleSpacing: 24.0,
-      actions: hasLogoutIcon == true
+      actions: widget.hasLogoutIcon == true
           ? [
               Container(
                 height: 50.0,
@@ -32,7 +113,7 @@ class GlintEventAuthAppbar extends StatelessWidget
                 ),
                 child: IconButton(
                   onPressed: () {
-                    // todo - add admin log out functionality
+                    _showLogoutDialog(context);
                   },
                   icon: const Icon(
                     Icons.power_settings_new_outlined,
@@ -46,7 +127,4 @@ class GlintEventAuthAppbar extends StatelessWidget
           : null,
     );
   }
-
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
--- lib/features/application/my_glint_app.dart
+++ lib/features/application/my_glint_app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:glint_frontend/design/exports.dart';
-import 'package:glint_frontend/features/admin/admin_event_live_screen.dart';
+import 'package:glint_frontend/features/admin/admin_dashboard_screen.dart';
 // import 'package:glint_frontend/navigation/glint_route_config.dart';
 
 class MyGlintApp extends StatelessWidget {
@@ -12,7 +12,7 @@ class MyGlintApp extends StatelessWidget {
       title: "Glint People App",
       debugShowCheckedModeBanner: false,
       theme: AppTheme.theme,
-      home: const AdminEventLiveScreen(),
+      home: const AdminDashboardScreen(),
       // routerConfig: glintMainRoutes,
       // routerConfig: glintMainRoutes,
     );
