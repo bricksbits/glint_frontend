da99823d04602e0718070230c317df362eb0b117
Abhishek <abhishekverma7t@gmail.com>
2025-02-13 16:08:26 +0530

    updated app bar action for admin dashboard. Added show menu for log out and edit profile

--- lib/design/components/admin/glint_event_auth_appbar.dart
+++ lib/design/components/admin/glint_event_auth_appbar.dart
@@ -20,6 +20,59 @@ class GlintEventAuthAppbar extends StatefulWidget
 }
 
 class _GlintEventAuthAppbarState extends State<GlintEventAuthAppbar> {
+  // show edit profile and logout popup
+  void _showProfileMenu(BuildContext context, RelativeRect position) {
+    showMenu(
+      context: context,
+      position: position,
+      color: AppColours.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      items: [
+        PopupMenuItem(
+          child: const Row(
+            children: [
+              Icon(Icons.edit, size: 20),
+              SizedBox(width: 12),
+              Text(
+                'Edit Profile',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+          onTap: () {
+            //todo - Handle navigation edit profile
+          },
+        ),
+        PopupMenuItem(
+          child: Row(
+            children: [
+              Icon(Icons.logout, size: 20, color: Colors.red[400]),
+              const SizedBox(width: 12),
+              Text(
+                'Logout',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.red[400],
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+          onTap: () {
+            // Handle logout
+            _showLogoutDialog(context);
+          },
+        ),
+      ],
+    );
+  }
+
+  // logout dialog pop up
   void _showLogoutDialog(BuildContext context) {
     showDialog(
       context: context,
@@ -105,21 +158,35 @@ class _GlintEventAuthAppbarState extends State<GlintEventAuthAppbar> {
       titleSpacing: 24.0,
       actions: widget.hasLogoutIcon == true
           ? [
-              Container(
-                height: 50.0,
-                width: 50.0,
-                decoration: BoxDecoration(
-                  color: AppColours.lightRed,
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: IconButton(
-                  onPressed: () {
-                    _showLogoutDialog(context);
-                  },
-                  icon: const Icon(
-                    Icons.power_settings_new_outlined,
-                    color: AppColours.red,
-                    size: 24.0,
+              GestureDetector(
+                onTap: () {
+                  final RenderBox button =
+                      context.findRenderObject() as RenderBox;
+                  final Offset offset = button.localToGlobal(Offset.zero);
+                  _showProfileMenu(
+                    context,
+                    RelativeRect.fromLTRB(
+                      MediaQuery.of(context).size.width - 200,
+                      offset.dy + button.size.height,
+                      MediaQuery.of(context).size.width - 10,
+                      offset.dy + button.size.height,
+                    ),
+                  );
+                },
+                child: Container(
+                  height: 50.0,
+                  width: 50.0,
+                  padding: const EdgeInsets.all(14.0),
+                  decoration: BoxDecoration(
+                    color: AppColours.backgroundShade,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: SvgPicture.asset(
+                    'lib/assets/icons/user_icon.svg',
+                    colorFilter: const ColorFilter.mode(
+                      AppColours.primaryBlue,
+                      BlendMode.srcIn,
+                    ),
                   ),
                 ),
               ),
