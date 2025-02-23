9c6d602a6ababe52836f82b7073f7beaba221ad0
Abhishek <abhishekverma7t@gmail.com>
2025-02-23 17:58:17 +0530

    Impl - Usecase implementation for Reset Password

--- lib/domain/application_logic/auth/reset_password_use_case.dart
+++ lib/domain/application_logic/auth/reset_password_use_case.dart
@@ -1,17 +1,37 @@
 import 'dart:async';
 
-import 'package:glint_frontend/domain/business_logic/repo/auth/authentication_repo.dart';
+import 'package:glint_frontend/data/remote/model/request/auth/reset_password_request_body.dart';
 import 'package:glint_frontend/domain/business_logic/repo/auth/forgot_password_repo.dart';
 import 'package:glint_frontend/utils/clean_arch_use_case.dart';
 
-class ResetPasswordUseCase extends UseCase<void, String> {
+import '../../../utils/network_response.dart';
+
+class ResetPasswordUseCase extends UseCase<void, ResetPasswordRequestBody> {
   final ForgotPasswordRepo forgotPasswordRepo;
 
   ResetPasswordUseCase(this.forgotPasswordRepo);
 
   @override
-  Future<Stream<void>> buildUseCaseStream(String? params) {
-    final StreamController<int> controller = StreamController();
-    throw UnimplementedError();
+  Future<Stream<void>> buildUseCaseStream(
+      ResetPasswordRequestBody? params) async {
+    final StreamController<NetworkResponse<bool>> controller =
+        StreamController();
+    try {
+      final loginResponse = await forgotPasswordRepo.resetPassword(params!);
+      switch (loginResponse) {
+        case Success():
+          controller.add(loginResponse.data);
+          logger.finest('GetUsersUseCase successful.');
+        case Failure():
+          controller.addError(loginResponse.error);
+          logger.severe('GetUsersUseCase unsuccessful.');
+          print(loginResponse.error);
+      }
+      controller.close();
+    } catch (caughtError) {
+      controller.addError(caughtError);
+    }
+
+    return controller.stream;
   }
 }
