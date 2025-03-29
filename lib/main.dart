bb2b7b8b0fcf6f912929edafc26106f689a122d3
glintOfficial <official@glintapp.io>
2025-03-29 16:35:38 +0530

    added : Firebase Core

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:glint_frontend/di/injection.dart';
 import 'package:glint_frontend/features/exports.dart';
+import 'package:glint_frontend/firebase_options.dart';
 import 'package:stream_chat_flutter_core/stream_chat_flutter_core.dart';
 
 Future<void> main() async{
@@ -18,6 +20,10 @@ Future<void> main() async{
   final channel = client.channel('messaging', id: 'flutterdevs');
 
   await channel.watch();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     MyGlintApp(
